57c1bf60e549fd374a6003292fdb7fffed094a38
Dale Dobson <ddobson445@gmail.com>
2017-02-18 15:55:44 -0500

    Fix typo in config development

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,7 +3,7 @@
 const config = {
   apiOrigins: {
     production: 'https://ga-wdi-boston.herokuapp.com',
-    development: 'http://localhost:4141',
+    development: 'http://localhost:4741',
   },
 };
 
