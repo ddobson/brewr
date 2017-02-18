0450fde25485542e32bd3317db97c1ca643b92da
Dale Dobson <ddobson445@gmail.com>
2017-02-18 15:45:16 -0500

    Trigger auth modals for sign-up & in

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -2,9 +2,34 @@
 
 const api = require('./api');
 const authUI = require('./ui');
+const getFormFields = require('../../../lib/get-form-fields');
+
+const parseActionId = function (id) {
+  return id.replace('-cta', '');
+};
+
+const onAuthPrompt = function (event) {
+  event.preventDefault();
+
+  const action = parseActionId(event.target.id);
+
+  authUI.triggerAuthModal(action);
+};
+
+// const onSignInPrompt = function (event) {
+//   event.preventDefault();
+//
+//   const action = parseActionId(event.target.id);
+// };
+
+// AJAX handlers //
 
 const onSignUp = function (event) {
   event.preventDefault();
+
+  const formData = getFormFields(event.target);
+
+  api.signUp(formData);
 };
 
 const onSignIn = function (event) {
@@ -19,7 +44,11 @@ const onChangePassword = function (event) {
   event.preventDefault();
 };
 
+// Event Handling
+
 const addHandlers = () => {
+  $('#content').on('click', '#sign-up-cta', onAuthPrompt);
+  $('#content').on('click', '#sign-in-cta', onAuthPrompt);
   $('#content').on('submit', '#sign-up', onSignUp);
   $('#content').on('submit', '#sign-in', onSignIn);
   $('#content').on('submit', '#sign-out', onSignOut);
--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const signUpModal = require('../templates/modals/sign-up-modal.handlebars');
+const signInModal = require('../templates/modals/sign-in-modal.handlebars');
+
+const triggerAuthModal = function (action) {
+  switch (action) {
+    case 'sign-up':
+      const signUpModalHtml = signUpModal();
+      $('.content').append(signUpModalHtml);
+      break;
+    case 'sign-in':
+      const signInModalHtml = signInModal();
+      $('.content').append(signInModalHtml);
+      break;
+    default:
+  }
+};
+
+module.exports = {
+  triggerAuthModal,
+};
